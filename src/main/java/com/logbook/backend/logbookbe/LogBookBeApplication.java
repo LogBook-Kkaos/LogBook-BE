2a5e614be5f5f5abb5d1b9fc18f5f39e4482bd68
주은 <00-10-21@naver.com>
2023-07-07 12:38:19 +0900

    feat: init BE project, connect with db

---
+++ src/main/java/com/logbook/backend/logbookbe/LogBookBeApplication.java
@@ -0,0 +1,13 @@
+package com.logbook.backend.logbookbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogBookBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogBookBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logbook/backend/logbookbe/LogBookBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logbook.backend.logbookbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogBookBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
