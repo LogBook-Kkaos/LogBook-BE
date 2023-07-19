bc952ab76e77e5799931b7e3b2445fd819255c71
서빈이 <70849467+leeseobin00@users.noreply.github.com>
2023-07-19 09:26:54 +0900

    feat: Implement Login API

---
+++ src/main/java/com/logbook/backend/logbookbe/domain/user/controller/dto/LoginRequest.java
@@ -0,0 +1,11 @@
+package com.logbook.backend.logbookbe.domain.user.controller.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LoginRequest {
+    private String email;
+    private String password;
+}
