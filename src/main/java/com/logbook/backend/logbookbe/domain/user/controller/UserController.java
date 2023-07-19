7fe5b39d0a6d7a4bcbf61e4c83ed84dd96057999
서빈이 <70849467+leeseobin00@users.noreply.github.com>
2023-07-19 22:10:37 +0900

    feat: Add Swagger Document to the User API

--- src/main/java/com/logbook/backend/logbookbe/domain/user/controller/UserController.java
+++ src/main/java/com/logbook/backend/logbookbe/domain/user/controller/UserController.java
@@ -1,20 +1,26 @@
 package com.logbook.backend.logbookbe.domain.user.controller;
 
+import com.logbook.backend.logbookbe.global.error.ErrorResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 import com.logbook.backend.logbookbe.domain.user.controller.dto.LoginRequest;
 import com.logbook.backend.logbookbe.domain.user.controller.dto.SignupRequest;
 import com.logbook.backend.logbookbe.domain.user.usecase.RefreshToken;
 import com.logbook.backend.logbookbe.domain.user.usecase.UserSignup;
 import com.logbook.backend.logbookbe.global.jwt.dto.JwtResponse;
-import com.logbook.backend.logbookbe.domain.user.model.User;
 import com.logbook.backend.logbookbe.domain.user.service.UserService;
 import com.logbook.backend.logbookbe.domain.user.usecase.UserLogin;
+
 import lombok.RequiredArgsConstructor;
-import org.hibernate.annotations.Parameter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,6 +41,13 @@ public class UserController {
     private final UserSignup userSignup;
     private final RefreshToken doRefreshToken;
 
+    @Operation(summary = "사용자 로그인", description = "사용자 계정으로 로그인합니다. RefreshToken은 Cookie에 자동으로 추가됩니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = JwtResponse.class))),
+            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @PostMapping("/login")
     public JwtResponse login(HttpServletResponse res, @Validated @RequestBody LoginRequest loginRequest) {
         JwtResponse jwt = userLogin.execute(loginRequest.getEmail(), loginRequest.getPassword());
@@ -45,11 +58,24 @@ public class UserController {
         return jwt;
     }
 
+    @Operation(summary = "사용자 토큰갱신", description = "AccessToken을 갱신합니다. 이 때 Cookie에 있는 RefreshToken을 자동으로 가져와 사용합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = JwtResponse.class))),
+            @ApiResponse(responseCode = "401", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "403", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @PostMapping("/refresh")
-    public JwtResponse refresh(@CookieValue("refreshToken") String refreshToken) {
+    public JwtResponse refresh(@Parameter(hidden = true) @CookieValue("refreshToken") String refreshToken) {
         return doRefreshToken.execute(refreshToken);
     }
 
+    @Operation(summary = "사용자 회원가입", description = "사용자 계정으로 회원가입합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = JwtResponse.class))),
+            @ApiResponse(responseCode = "409", content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+            @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+    })
     @PostMapping("/register")
     public JwtResponse signup(HttpServletResponse res, @Validated @RequestBody SignupRequest signupRequest) {
         JwtResponse jwt = userSignup.execute(signupRequest.getUserName(), signupRequest.getEmail(), signupRequest.getDepartment(), signupRequest.getPassword());
