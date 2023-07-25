b4cefb3a179996bfef07b231483d27257e089799
서빈이 <70849467+leeseobin00@users.noreply.github.com>
2023-07-25 10:53:33 +0900

    feat: Implement oauth detail

---
+++ src/main/java/com/logbook/backend/logbookbe/global/oauth/OAuthDetails.java
@@ -0,0 +1,66 @@
+package com.logbook.backend.logbookbe.global.oauth;
+
+import com.logbook.backend.logbookbe.global.jwt.AuthRole;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+@Getter
+@AllArgsConstructor
+public class OAuthDetails implements UserDetails, OAuth2User {
+    private final String email;
+    private final String name;
+    private final Map<String, Object> attributes;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singletonList(new SimpleGrantedAuthority(AuthRole.ROLE_ADMIN.getRole()));
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
