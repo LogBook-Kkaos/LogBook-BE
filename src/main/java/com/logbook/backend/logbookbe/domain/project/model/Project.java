8dd52fb5cf5bfd613941ec0b5701373f876853d0
서빈이 <70849467+leeseobin00@users.noreply.github.com>
2023-08-02 16:19:50 +0900

    style: edit project_title to project_name

--- src/main/java/com/logbook/backend/logbookbe/domain/project/model/Project.java
+++ src/main/java/com/logbook/backend/logbookbe/domain/project/model/Project.java
@@ -16,7 +16,7 @@ public class Project {
     @Column(name="project_id")
     private Integer projectId;
 
-    @Column(name="project_title", nullable = false)
+    @Column(name="project_name", nullable = false)
     private String projectName;
 
     @Lob
