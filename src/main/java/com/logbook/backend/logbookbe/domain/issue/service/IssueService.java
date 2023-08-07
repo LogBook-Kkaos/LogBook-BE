11fc801bf7c9aabf4fb1fabbc317903a1a694e77
주은 <00-10-21@naver.com>
2023-08-07 15:50:11 +0900

    fix: change function name match with convention

--- src/main/java/com/logbook/backend/logbookbe/domain/issue/service/IssueService.java
+++ src/main/java/com/logbook/backend/logbookbe/domain/issue/service/IssueService.java
@@ -1,18 +1,23 @@
 
 package com.logbook.backend.logbookbe.domain.issue.service;
 
-import com.logbook.backend.logbookbe.domain.issue.controller.dto.IssueDeleteResponse;
+import com.logbook.backend.logbookbe.domain.document.dto.createDocumentRequest;
+import com.logbook.backend.logbookbe.domain.document.model.Document;
+import com.logbook.backend.logbookbe.domain.issue.controller.dto.createIssueRequest;
+import com.logbook.backend.logbookbe.domain.issue.controller.dto.deleteIssueResponse;
 import com.logbook.backend.logbookbe.domain.issue.model.Issue;
 import com.logbook.backend.logbookbe.domain.issue.repository.IssueRepository;
 import com.logbook.backend.logbookbe.domain.issue.type.Status;
-import com.logbook.backend.logbookbe.domain.project.controller.dto.DeleteResponse;
 import com.logbook.backend.logbookbe.domain.project.model.Project;
 import com.logbook.backend.logbookbe.domain.project.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
@@ -33,13 +38,26 @@ public class IssueService {
                 .orElseThrow(() -> new NoSuchElementException("해당하는 이슈를 찾을 수 없습니다."));
     }
 
-    public Issue createIssue(UUID projectId, Issue issue) {
-        return projectRepository.findById(projectId)
-                .map(project -> {
-                    issue.setProject((Project) project);
-                    return issueRepository.save(issue);
-                })
-                .orElseThrow(() -> new NoSuchElementException("해당하는 프로젝트를 찾을 수 없습니다."));
+    public boolean createIssue(createIssueRequest issueDto, UUID projectId) {
+        Issue issue = new Issue();
+        issue.setAssignee(issueDto.getAssignee());
+        issue.setIssueTitle(issueDto.getIssueTitle());
+        issue.setIssueDescription(issueDto.getIssueDescription());
+        issue.setStatus(issueDto.getStatus());
+        issue.setStartDate(issueDto.getStartDate());
+        issue.setEndDate(issueDto.getEndDate());
+
+        Optional<Project> projectOptional = projectRepository.findByProjectId(projectId);
+
+        if (projectOptional.isPresent()) {
+            issue.setProject(projectOptional.get());
+        } else {
+            throw new RuntimeException("해당하는 프로젝트가 없습니다.");
+        }
+
+        issueRepository.save(issue);
+
+        return true;
     }
 
     public Issue updateIssue(UUID issueId, Issue updatedIssue) {
@@ -54,9 +72,9 @@ public class IssueService {
                 .orElseThrow(() -> new NoSuchElementException("해당하는 이슈를 찾을 수 없습니다."));
     }
 
-    public IssueDeleteResponse deleteIssue(UUID issueId) {
+    public deleteIssueResponse deleteIssue(UUID issueId) {
         issueRepository.deleteById(issueId);
-        return new IssueDeleteResponse(issueId, "이슈가 성공적으로 삭제되었습니다.");
+        return new deleteIssueResponse(issueId, "이슈가 성공적으로 삭제되었습니다.");
     }
 
     public List<Issue> filterIssues(UUID projectId, UUID assigneeId, Status status) {
