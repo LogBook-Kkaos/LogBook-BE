05a915160b9ec69a29666aae016bbaf55ba5b1b9
jykyeong99 <2009skylove@naver.com>
2023-08-08 11:19:42 +0900

    feat: MyProject API

--- src/main/java/com/logbook/backend/logbookbe/domain/member/service/MemberService.java
+++ src/main/java/com/logbook/backend/logbookbe/domain/member/service/MemberService.java
@@ -2,10 +2,12 @@ package com.logbook.backend.logbookbe.domain.member.service;
 
 import com.logbook.backend.logbookbe.domain.member.model.Member;
 import com.logbook.backend.logbookbe.domain.member.repository.MemberRepository;
+import com.logbook.backend.logbookbe.domain.project.model.Project;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class MemberService {
@@ -20,4 +22,6 @@ public class MemberService {
     public void createMember(Member member){
         memberRepository.save(member);
     }
+
+    public List<Project> findMyProject(UUID userId) { return memberRepository.findProjectsByUserId(userId); }
 }
