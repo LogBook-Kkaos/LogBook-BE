c14b05a89469cb2d30237f98b1d02a5da3d8b8f2
주은 <00-10-21@naver.com>
2023-08-08 17:55:36 +0900

    refactor: Modify Release Note repository to get UUID

--- src/main/java/com/logbook/backend/logbookbe/domain/releaseNote/repository/ReleaseNoteRepository.java
+++ src/main/java/com/logbook/backend/logbookbe/domain/releaseNote/repository/ReleaseNoteRepository.java
@@ -1,11 +1,20 @@
 package com.logbook.backend.logbookbe.domain.releaseNote.repository;
 
+import com.logbook.backend.logbookbe.domain.document.model.Document;
 import com.logbook.backend.logbookbe.domain.releaseNote.model.ReleaseNote;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
-public interface ReleaseNoteRepository extends JpaRepository<ReleaseNote, Long> {
+public interface ReleaseNoteRepository extends JpaRepository<ReleaseNote, UUID> {
+
+    List<ReleaseNote> findByProjectProjectId(UUID projectId);
+
+    ReleaseNote findByReleaseNoteId(UUID releaseNoteId);
 
 }
 
