af23a5678126d9416989788ea8c483e01c4f0bc6
001021 <00-10-21@naver.com>
2023-08-10 22:42:00 +0900

    feat: Change type to UUID

--- src/main/java/com/logbook/backend/logbookbe/releaseContent/repository/ReleaseContentRepository.java
+++ src/main/java/com/logbook/backend/logbookbe/releaseContent/repository/ReleaseContentRepository.java
@@ -4,6 +4,8 @@ import com.logbook.backend.logbookbe.releaseContent.model.ReleaseContent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface ReleaseContentRepository extends JpaRepository<ReleaseContent, Long> {
+public interface ReleaseContentRepository extends JpaRepository<ReleaseContent, UUID> {
 }
