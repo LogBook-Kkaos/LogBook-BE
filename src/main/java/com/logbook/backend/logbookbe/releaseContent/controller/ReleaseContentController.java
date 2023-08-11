fb0ab0e783a3645d2691ab97c419bec2d60dbbee
001021 <00-10-21@naver.com>
2023-08-11 10:52:55 +0900

    feat: Create Release content controller

--- src/main/java/com/logbook/backend/logbookbe/releaseContent/controller/ReleaseContentController.java
+++ src/main/java/com/logbook/backend/logbookbe/releaseContent/controller/ReleaseContentController.java
@@ -1,34 +1,64 @@
 package com.logbook.backend.logbookbe.releaseContent.controller;
 
-import com.logbook.backend.logbookbe.releaseContent.controller.dto.ReleaseContentDto;
+import com.logbook.backend.logbookbe.domain.releaseNote.controller.dto.CreateReleaseNoteRequest;
+import com.logbook.backend.logbookbe.domain.releaseNote.controller.dto.DeleteReleaseNoteResponse;
+import com.logbook.backend.logbookbe.domain.releaseNote.controller.dto.GetReleaseNoteResponse;
+import com.logbook.backend.logbookbe.domain.releaseNote.service.ReleaseNoteService;
+import com.logbook.backend.logbookbe.releaseContent.controller.dto.CreateReleaseContentRequest;
+import com.logbook.backend.logbookbe.releaseContent.controller.dto.DeleteReleaseContentResponse;
+import com.logbook.backend.logbookbe.releaseContent.controller.dto.GetReleaseContentResponse;
 import com.logbook.backend.logbookbe.releaseContent.model.ReleaseContent;
 import com.logbook.backend.logbookbe.releaseContent.repository.ReleaseContentRepository;
+import com.logbook.backend.logbookbe.releaseContent.service.ReleaseContentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/release-notes/release-content")
+@RequestMapping("/api/projects/{projectId}/release_notes/{releaseNoteId}/release_contents")
 public class ReleaseContentController {
 
     @Autowired
-    private ReleaseContentRepository releaseContentRepository;
+    private ReleaseContentService releaseContentService;
 
-    @PostMapping("/create")
-    public ResponseEntity<String> createReleaseContent(@RequestBody ReleaseContentDto releaseContentDto) {
-        ReleaseContent releaseContent = new ReleaseContent();
-        releaseContent.setReleaseNoteId(releaseContentDto.getReleaseNoteId());
-        releaseContent.setDocumentId(releaseContentDto.getDocumentId());
-        releaseContent.setReleaseSummary(releaseContentDto.getReleaseSummary());
-        releaseContent.setCategory(releaseContentDto.getCategory());
-        releaseContent.setDocumentLink(releaseContentDto.getDocumentLink());
+    @GetMapping
+    public ResponseEntity<List<GetReleaseContentResponse>> getAllReleaseContent(@PathVariable("releaseNoteId") UUID releaseNoteId) {
+        List<GetReleaseContentResponse> getAllReleaseContents = releaseContentService.getAllReleaseContents(releaseNoteId);
+        return ResponseEntity.ok(getAllReleaseContents);
+    }
 
-        releaseContentRepository.save(releaseContent);
+    @GetMapping("/{releaseContentId}")
+    public ResponseEntity<GetReleaseContentResponse> getReleaseContentById(@PathVariable UUID releaseNoteId, @PathVariable UUID releaseContentId) {
+        GetReleaseContentResponse getReleaseContentById = releaseContentService.getReleaseContentById(releaseContentId);
+        return new ResponseEntity<GetReleaseContentResponse>(getReleaseContentById, HttpStatus.CREATED);
+    }
 
-        return ResponseEntity.ok("Release content created successfully.");
+    @PostMapping
+    public UUID createReleaseContent(@RequestBody CreateReleaseContentRequest releaseContentDTO, @PathVariable UUID releaseNoteId) {
+        UUID CreateReleaseContentRequest = releaseContentService.createReleaseContent(releaseContentDTO, releaseNoteId);
+        return CreateReleaseContentRequest;
+    }
+
+    @PutMapping("/{releaseContentId}")
+    public ResponseEntity<CreateReleaseContentRequest> updateReleaseContent(@PathVariable("releaseContentId") UUID releaseContentId,
+                                                                      @Valid @RequestBody CreateReleaseContentRequest updatedReleaseContent) {
+        CreateReleaseContentRequest updatedReleaseContentResult = releaseContentService.updateReleaseContent(releaseContentId, updatedReleaseContent);
+        return new ResponseEntity<CreateReleaseContentRequest>(updatedReleaseContentResult, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{releaseContentId}")
+    public DeleteReleaseContentResponse deleteReleaseContent(@PathVariable UUID releaseContentId) {
+        return releaseContentService.deleteReleaseContent(releaseContentId);
     }
 
 
