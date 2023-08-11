d93a3dbd95eb487c8862d9cb629ff549083645f9
001021 <00-10-21@naver.com>
2023-08-11 10:51:52 +0900

    feat: Add findByReleaseContentId method

--- src/main/java/com/logbook/backend/logbookbe/releaseContent/repository/ReleaseContentRepository.java
+++ src/main/java/com/logbook/backend/logbookbe/releaseContent/repository/ReleaseContentRepository.java
@@ -1,11 +1,18 @@
 package com.logbook.backend.logbookbe.releaseContent.repository;
 
+import com.logbook.backend.logbookbe.domain.releaseNote.model.ReleaseNote;
 import com.logbook.backend.logbookbe.releaseContent.model.ReleaseContent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface ReleaseContentRepository extends JpaRepository<ReleaseContent, UUID> {
+
+    List<ReleaseContent> findByReleaseNoteReleaseNoteId(UUID releaseNoteId);
+
+    ReleaseContent findByReleaseContentId(UUID releaseContentId);
+
 }
