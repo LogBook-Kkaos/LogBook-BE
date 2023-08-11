971f8439a4a27f4e4de9e632c406b202e4a9362f
서빈이 <70849467+leeseobin00@users.noreply.github.com>
2023-08-11 12:25:12 +0900

    test: User Service Test code

--- src/main/java/com/logbook/backend/logbookbe/domain/user/controller/dto/LoginRequest.java
+++ src/main/java/com/logbook/backend/logbookbe/domain/user/controller/dto/LoginRequest.java
@@ -2,8 +2,10 @@ package com.logbook.backend.logbookbe.domain.user.controller.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class LoginRequest {
     private String email;
